7d9168c2a96f57764b5603b740ef3099ce313673
lavisha-rajwadda <lavisharajwadda@gmail.com>
2024-11-03 10:46:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// You can add interactivity here if needed. For example:
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
